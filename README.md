4f2b7ab585f0fff1ee41c3080778f0605dc39c6d
letamin <lethanhminh0901@gmail.com>
2020-07-31 01:24:04 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# D3---Temperature
+Example for D3 - Temperature
---
+++ js/script.js
@@ -0,0 +1,106 @@
+getData()
+
+function getData() {
+    d3.csv('data/data.csv')  //data is downloaded from https://population.un.org/wpp/Download/Standard/Population/
+        .then(data => {
+            data.forEach(row => {
+                row.Population = +row.Population * 1000;
+                row.Country = row.Country
+            });
+            render(data);
+        })
+        .catch(err => console.log(err))
+}
+
+const render = (data) => {
+    const chartData = initializeChart();
+    const { xValue, yValue, margin, innerWidth, innerHeight, svg } = chartData;
+
+    const scales = scale(innerWidth, innerHeight, data, xValue, yValue);
+    const { xScale, yScale } = scales;
+
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    addingAxes(xScale, yScale, innerWidth, innerHeight, g);
+    addingTitle(g, innerWidth);
+
+    g.selectAll('.bar').data(data)
+        .enter().append('rect').attr("class", "bar")
+        .attr('x', d => xScale(xValue(d)))
+        .attr('y', d => yScale(yValue(d)))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => innerHeight - yScale(yValue(d)))
+};
+
+//Adding the X and Y axes and their labels
+const addingAxes = (xScale, yScale, innerWidth, innerHeight, g) => {
+    const xAxisLabel = 'Country';
+    const yAxisLabel = 'Population';
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickPadding(10);
+
+    const formatNumberYaxis = number => d3.format('.2s')(number).replace('G', 'B') //format the numbers for Y Axis
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat(formatNumberYaxis)
+        .tickSize(-innerWidth)
+        .tickPadding(5);
+
+    const yAxisG = g.append('g').call(yAxis);
+    const xAxisG = g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${innerHeight})`);
+
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', -50)
+        .attr('x', -innerHeight / 2)
+        .attr('text-anchor', 'middle')
+        .attr('transform', `rotate(-90)`)
+        .text(yAxisLabel);
+
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 50)
+        .attr('x', innerWidth / 2)
+        .text(xAxisLabel);
+}
+
+//Initialize some data for our chart
+const initializeChart = () => {
+    const svg = d3.select('svg');
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+    const xValue = d => d.Country;
+    const yValue = d => d.Population;
+    const margin = { top: 120, right: 40, bottom: 88, left: 150 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    return { xValue, yValue, margin, innerWidth, innerHeight, svg }
+}
+
+//Adding the title for our chart
+const addingTitle = (g, innerWidth) => {
+    const title = 'Top 15 contries with highest population';
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -20)
+        .attr('x', innerWidth / 3.6)
+        .text(title);
+}
+
+//Scales the values from our data to the screen
+const scale = (innerWidth, innerHeight, data, xValue, yValue) => {
+    const xScale = d3.scaleBand()
+        .domain(data.map(xValue))
+        .range([0, innerWidth])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, yValue)])
+        .range([innerHeight, 0])
+
+    return { xScale, yScale }
+}
+
+
